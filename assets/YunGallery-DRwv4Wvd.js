71c2f79df9955f7a3e81f33fccf6da6c80ac10cf
YDTJv <YDTJv@users.noreply.github.com>
2024-08-11 15:19:55 +0000

    deploy: cff5ead44fed815aa55283a841b489522aa33cf6

---
+++ assets/YunGallery-DRwv4Wvd.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-BTba3KPS.js";import"./app-0iLCH2x9.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-C9UHE0Lj.js";export{o as default};
---
+++ assets/back-BXQjWbBm.js
@@ -0,0 +1,1 @@
+import{u as o}from"./app-0iLCH2x9.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
