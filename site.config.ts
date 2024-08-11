0a0dfd9f26e44e397cbf762d67086aa66e05f579
YDTGv <2595190770@qq.com>
2024-08-11 22:44:48 +0800

    v2.0推送

--- site.config.ts
+++ site.config.ts
@@ -1,20 +1,47 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
+
+  /**
+   * 基础配置
+   */
   url: 'https://valaxy.site/',
   lang: 'zh-CN',
-  title: '一岛糖桔 | Blog',
+  title: '一岛糖桔 | Blog',       // 网站标题
   author: {
-    name: '一岛糖桔',
+    name: '一岛糖桔',                                    // 头像下方名
+    avatar: 'https://www.ydtjv.cn/images/avatar.svg',   // 头像
   },
-  description: '给我打电话吧，叫我出去吧！带我去散步在路灯下，牵我的手。',
+
+  /**
+   * 副标题
+   */
+  subtitle: '',
+  description: '许我人间一两风，填我十万八千梦。',       // 描述
+
+  /**
+   * 站点图标
+   */
+  favicon: 'https://www.ydtjv.cn/favicon.svg',
+
+  /**
+   * 所有文章默认的 Frontmatter
+   */
+  frontmatter: {
+    time_warning: false,      // 显示阅读时间警告
+    // password: "123123"        // 内容加密的密码
+  },
+
+  /**
+   * 社交信息
+   */
   social: [
-    {
-      name: 'RSS',
-      link: '/atom.xml',
-      icon: 'i-ri-rss-line',
-      color: 'orange',
-    },
+    // {
+    //   name: 'RSS',
+    //   link: '/atom.xml',
+    //   icon: 'i-ri-rss-line',
+    //   color: 'orange',
+    // },
     {
       name: 'QQ 2595190770',
       link: 'https://ydtjv.cn',
@@ -27,94 +54,155 @@ export default defineSiteConfig({
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
-    {
-      name: '微博',
-      link: 'https://weibo.com/jizhideyunyoujun',
-      icon: 'i-ri-weibo-line',
-      color: '#E6162D',
-    },
-    {
-      name: '豆瓣',
-      link: 'https://www.douban.com/people/yunyoujun/',
-      icon: 'i-ri-douban-line',
-      color: '#007722',
-    },
+    // {
+    //   name: '微博',
+    //   link: 'https://weibo.com/jizhideyunyoujun',
+    //   icon: 'i-ri-weibo-line',
+    //   color: '#E6162D',
+    // },
+    // {
+    //   name: '豆瓣',
+    //   link: 'https://www.douban.com/people/yunyoujun/',
+    //   icon: 'i-ri-douban-line',
+    //   color: '#007722',
+    // },
     {
       name: '网易云音乐',
-      link: 'https://music.163.com/#/user/home?id=247102977',
+      link: 'https://music.163.com/#/user/home?id=4904950084',
       icon: 'i-ri-netease-cloud-music-line',
       color: '#C20C0C',
     },
-    {
-      name: '知乎',
-      link: 'https://www.zhihu.com/people/yunyoujun/',
-      icon: 'i-ri-zhihu-line',
-      color: '#0084FF',
-    },
+    // {
+    //   name: '知乎',
+    //   link: 'https://www.zhihu.com/people/yunyoujun/',
+    //   icon: 'i-ri-zhihu-line',
+    //   color: '#0084FF',
+    // },
     {
       name: '哔哩哔哩',
-      link: 'https://space.bilibili.com/1579790',
+      link: 'https://space.bilibili.com/635106786',
       icon: 'i-ri-bilibili-line',
       color: '#FF8EB3',
     },
-    {
-      name: '微信公众号',
-      link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
-      icon: 'i-ri-wechat-2-line',
-      color: '#1AAD19',
-    },
-    {
-      name: 'Twitter',
-      link: 'https://twitter.com/YunYouJun',
-      icon: 'i-ri-twitter-line',
-      color: '#1da1f2',
-    },
-    {
-      name: 'Telegram Channel',
-      link: 'https://t.me/elpsycn',
-      icon: 'i-ri-telegram-line',
-      color: '#0088CC',
-    },
+    // {
+    //   name: '微信公众号',
+    //   link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
+    //   icon: 'i-ri-wechat-2-line',
+    //   color: '#1AAD19',
+    // },
+    // {
+    //   name: 'Twitter',
+    //   link: 'https://twitter.com/YunYouJun',
+    //   icon: 'i-ri-twitter-line',
+    //   color: '#1da1f2',
+    // },
+    // {
+    //   name: 'Telegram Channel',
+    //   link: 'https://t.me/elpsycn',
+    //   icon: 'i-ri-telegram-line',
+    //   color: '#0088CC',
+    // },
     {
       name: 'E-Mail',
-      link: 'mailto:me@yunyoujun.cn',
+      link: 'http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=d0VCTkJGTkdAQEc3BgZZFBga',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
     },
-    {
-      name: 'Travelling',
-      link: 'https://www.travellings.cn/go.html',
-      icon: 'i-ri-train-line',
-      color: 'var(--va-c-text)',
-    },
+    // {
+    //   name: 'Travelling',
+    //   link: 'https://www.travellings.cn/go.html',
+    //   icon: 'i-ri-train-line',
+    //   color: 'var(--va-c-text)',
+    // },
   ],
 
   search: {
     enable: false,
   },
 
+  /**
+   * 赞助（打赏）信息
+   */
   sponsor: {
     enable: true,
-    title: '我很可爱，请给我钱！',
+    title: '赏杯汽水喝呗！',
     methods: [
       {
         name: '支付宝',
-        url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
+        url: 'https://cdn.yunyoujun.cn/images/donate/zfb_donate.jpg',
         color: '#00A3EE',
         icon: 'i-ri-alipay-line',
       },
-      {
-        name: 'QQ 支付',
-        url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
-        color: '#12B7F5',
-        icon: 'i-ri-qq-line',
-      },
+      // {
+      //   name: 'QQ 支付',
+      //   url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
+      //   color: '#12B7F5',
+      //   icon: 'i-ri-qq-line',
+      // },
       {
         name: '微信支付',
-        url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
+        url: 'https://cdn.yunyoujun.cn/images/donate/vx_donate.jpg',
         color: '#2DC100',
         icon: 'i-ri-wechat-pay-line',
       },
     ],
   },
+
+  /**
+   * 开启阅读统计
+   */
+  statistics: {
+    enable: true,
+    readTime: {
+      /**
+       * 阅读速度
+       */
+      speed: {
+        cn: 300,
+        en: 200,
+      },
+    },
+  },
+
+  /**
+   * 设置代码块高度限制
+   */
+  codeHeightLimit: 300,      // 代码块高度不会超过 300px，并自动折叠。
+
+  /**
+   * 内容加密
+   */
+  encrypt: {
+    enable: false,
+  },
+
+  /**
+   * 客户端重定向
+   */
+  // redirects: {
+  //   useVueRouter: true,
+  //   rules: [
+  //     {
+  //       from: ['/foo', '/bar'],
+  //       to: '/about',
+  //     },
+  //     {
+  //       from: '/v1/about',
+  //       to: '/about',
+  //     },
+  //   ]
+  // },
+
+  /**
+   * 图片预览（Medium Zoom）
+   */
+  mediumZoom: { enable: false },     // 内置 medium-zoom 进行图片预览，默认关闭
+
+  /**
+   * 懒加载 Vanilla Lazyload
+   */
+  vanillaLazyload: {
+    // 默认不开启
+    enable: true,
+  }
 })
--- valaxy.config.ts
+++ valaxy.config.ts
@@ -6,44 +6,124 @@ const safelist = [
   'i-ri-home-line',
 ]
 
-/**
- * User Config
- */
-export default defineValaxyConfig<UserThemeConfig>({
-  // site config see site.config.ts
 
+export default defineValaxyConfig<UserThemeConfig>({
+  /**
+   * 主题名
+   */
   theme: 'yun',
 
+  /**
+   * 主题配置
+   */
   themeConfig: {
     banner: {
       enable: true,
-      title: '一岛糖桔的小站',
+      title: '一岛糖桔的鲸岛',
+      // 手动分割
+      // title: ['一岛糖桔的', '鲸岛'],
       cloud: {
-        enable: true,
+        enable: true,    // 首页下方的动态云
       },
     },
 
+    /**
+     * 主题配色
+     */
+    colors: {
+      primary: '#0078E7'     // @ default '#0078E7'
+    },
+
+    /**
+     * 背景图
+     */
+    bg_image: {
+      enable: true,
+      url: 'https://www.ydtjv.cn/images/background/bg_white.jpg',     // 日间
+      dark: 'https://www.ydtjv.cn/images/background/bg_black.jpg',    // 夜间
+      opacity: 0.6,                                                   // 透明度
+    },
+
+
+    /**
+     * 自定义页面（显示在社交导航栏下方）
+     */
     pages: [
       {
-        name: '我的小伙伴们',
-        url: '/links/',
+        name: '我的小可爱们',
+        url: '/links/',                 // 网页文件位于 pages/links/index.md
         icon: 'i-ri-genderless-line',
         color: 'dodgerblue',
       },
-      {
-        name: '喜欢的女孩子',
-        url: '/girls/',
-        icon: 'i-ri-women-line',
-        color: 'hotpink',
-      },
+      // {
+      //   name: '喜欢的女孩子',
+      //   url: '/girls/',
+      //   icon: 'i-ri-women-line',
+      //   color: 'hotpink',
+      // },
     ],
 
-    footer: {
-      since: 2016,
-      beian: {
+    /**
+     * 说点什么
+     * 自定义 API 链接，在 public 下新建 json 的方式来使用，public/saywords.json
+     * ["xxxx", "xxxxx"]
+     */
+    say: {
+      enable: false,
+      api: 'https://www.ydtjv.cn/api/words',
+      hitokoto: {            // https://hitokoto.cn/
         enable: true,
-        icp: '苏ICP备17038157号',
-      },
+        api: 'https://v1.hitokoto.cn/?encode=json',
+      }
+    },
+
+    /**
+     * 公告
+     */
+    notice: {
+      enable: false,
+      hideInPages: true,     // 在 /pages/[page] 中隐藏
+      content: "这是一条公告"
+    },
+
+    /**
+     * 点击时的烟花效果
+     */
+    fireworks: {
+      enable: true,
+      /**
+       * 烟花颜色
+       * 默认 ['#66A7DD', '#3E83E1', '#214EC2']
+       */
+      colors: ['#66A7DD', '#3E83E1', '#214EC2'],
+    },
+
+
+    /**
+     * 页脚
+     */
+    footer: {
+      /**
+       * Powered by valaxy & valaxy-theme-${name}, default is yun
+       */
+      powered: true,
+
+      /**
+       * 建站时间
+       */
+      since: 2024,
+
+
+      // beian: {
+      //   enable: true,
+      //   /**
+      //    * Chinese Users | 中国用户
+      //    * 备案 ICP
+      //    * 国内用户需要在网站页脚展示备案 ICP 号
+      //    * https://beian.miit.gov.cn/
+      //    */
+      //   icp: '苏ICP备17038157号',
+      // },
     },
   },
 
